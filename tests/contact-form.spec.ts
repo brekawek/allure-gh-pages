49c33c6ff8fa87d2850b9901d617fdafdf77fe08
rekawba <brekawek9@gmail.com>
2026-03-08 17:41:02 +0100

    fix

--- tests/contact-form.spec.ts
+++ tests/contact-form.spec.ts
@@ -5,31 +5,28 @@ test.describe('Test describe', () => {
         await page.goto('');
 
         await expect(page).toHaveTitle(
-            'Playwright enables reliable end-to-end testing for modern web apps.',
+            'Fast and reliable end-to-end testing for modern web apps | Playwright',
         );
     });
 
     test('always error', async ({ page }) => {
         await page.goto('');
 
-        await expect(page).toHaveTitle(
-            'Playwright enables reliable end-to-end testing for modern web apps.',
-        );
+        await expect(page).toHaveTitle('34534534534534534534');
     });
 
     test('random success/error', async ({ page }) => {
-        await page.goto('');
-
         const isSuccess = Math.random() > 0.5;
         console.log(`isSuccess: ${isSuccess}`);
+
+        await page.goto('');
+
         if (isSuccess) {
             await expect(page).toHaveTitle(
-                'Playwright enables reliable end-to-end testing for modern web apps.',
+                'Fast and reliable end-to-end testing for modern web apps | Playwright',
             );
         } else {
-            await expect(page).toHaveTitle(
-                'Playwright enables reliable end-to-end testing for modern web apps.',
-            );
+            await expect(page).toHaveTitle('34534534534534534534');
         }
     });
 });
